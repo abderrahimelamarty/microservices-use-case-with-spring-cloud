3f0e7b2f9358b397eea03a1aa60d122bec487a7f
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-04-03 00:22:41 +0000

    first_commit

---
+++ inventory-service/src/main/java/com/abdo/inventoryservice/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.abdo.inventoryservice.repositories;
+
+import com.abdo.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
